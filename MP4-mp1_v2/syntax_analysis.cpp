ec5adb3f5bac8af9d8d2b8f39fe42ed148bdb319
raretutal <retutal.ryananthony@gmail.com>
2026-03-24 22:35:12 +0800

    added checkVariableDeclaration DFA transition table

--- MP4-mp1_v2/syntax_analysis.cpp
+++ MP4-mp1_v2/syntax_analysis.cpp
@@ -14,3 +14,27 @@ int getTokenIndex(TokenType type) {
         default:            return -1;
     }
 }
+
+bool checkVariableDeclaration(const std::vector<Token>& tokens) {
+    // Columns: TYPE, ID, =, VAL, COMMA, SEMI, (, )
+    int table[7][8] = {
+        { 1, -1, -1, -1, -1, -1, -1, -1}, // State 0: Start
+        {-1,  2, -1, -1, -1, -1, -1, -1}, // State 1: Seen Type
+        {-1, -1,  3, -1,  5,  6, -1, -1}, // State 2: Seen ID
+        {-1,  4, -1,  4, -1, -1, -1, -1}, // State 3: Seen =
+        {-1, -1,  3, -1,  5,  6, -1, -1}, // State 4: Seen Value
+        {-1,  2, -1, -1, -1, -1, -1, -1}, // State 5: Seen Comma
+        { 1, -1, -1, -1, -1, -1, -1, -1}  // State 6: Accepting (Semi)
+    };
+
+    int state = 0;
+    for (const auto& token : tokens) {
+        int input = getTokenIndex(token.type);
+        if (input == -1) return false;
+
+        state = table[state][input];
+        if (state == -1) return false;
+    }
+    
+    return (state == 6);
+}
