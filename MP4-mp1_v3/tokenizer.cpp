0fc3ac7fb8b4f7b6db53b9635d91344dd976f6b8
raretutal <retutal.ryananthony@gmail.com>
2026-04-27 16:13:19 +0800

    removed useless ahhh code before

--- MP4-mp1_v3/tokenizer.cpp
+++ MP4-mp1_v3/tokenizer.cpp
@@ -5,56 +5,6 @@
 #include <vector>
 #include <algorithm>
 
-bool isType(const std::string& s){
-    return (s == "int" || s == "float" || s == "double" || s == "char"
-    || s == "void");
-}
-
-bool isNumber(const std::string& s){
-    if (s.empty()) return false;
-
-    bool decimalPointSeen = false;
-    size_t start = 0;
-
-    if (s[0] == '-'){
-        if (s.length() == 1) return false;
-        start = 1;
-    }
-
-    for (size_t i = start; i < s.length(); i++){
-        if (s[i] == '.') {
-            if (decimalPointSeen) return false;
-            decimalPointSeen = true;
-        } else if (!isdigit(s[i])){
-            return false;
-        }
-    
-    }
-    return true;
-}
-
-bool isValidIdentifier(const std::string& s){
-    if (s.empty()) return false;
-
-    // Rule 1:: Start with a letter or underscore
-    if (!isalpha(s[0]) && s[0] != '_'){
-        return false;
-    }
-
-    // Rule 2: the rest are just letters, numbers, or underscores
-    for (size_t i = 1; i< s.length(); i++){
-        if(!isalnum(s[i]) && s[i] != '_'){
-            return false;
-        }
-    }
-
-    //Rule 3: Keywords can't be identifiers
-    if(isType(s)) return false;
-
-    return true;
-
-}
-
     // ~MAIN TOKENIZE FUNCTION~
 
 std::vector<Token> tokenize(std::string code){
