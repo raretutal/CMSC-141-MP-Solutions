ba17300aedb01e0ba6556e77ad9bdb23d504b131
raretutal <retutal.ryananthony@gmail.com>
2026-04-30 01:49:03 +0800

    added implementation for parsing OPERAND, L_PARENTHESIS, and R_PARENTHESIS

--- MP4-mp1_v3/parser.cpp
+++ MP4-mp1_v3/parser.cpp
@@ -10,7 +10,23 @@ int getPrecedence(TokenType type){
 }
 
 std::vector<Token> infixToPostfix(const std::vector<Token>& infix_token){
+    
+    std::vector<Token> output_queue;
+    std::vector<Token> operator_stack;
 
+    for(const Token& token: infix_token){
+        if( token.type == OPERAND){
+            output_queue.push_back(token);
+
+        }else if(token.type == L_PARENTHESIS){
+            operator_stack.push_back(token);
+
+        }else if(token.type == R_PARENTHESIS){
+
+            
+
+        }
+    }
     
 }
 
