e5974df6d5460ba6ac32f6a754353030beb36492
raretutal <retutal.ryananthony@gmail.com>
2026-04-30 08:29:07 +0800

    finished the rest of the parser implementation from infix to postfix

--- MP4-mp1_v3/parser.cpp
+++ MP4-mp1_v3/parser.cpp
@@ -22,10 +22,29 @@ std::vector<Token> infixToPostfix(const std::vector<Token>& infix_token){
             operator_stack.push_back(token);
 
         }else if(token.type == R_PARENTHESIS){
+            while(!operator_stack.empty() && operator_stack.back().type != L_PARENTHESIS){
+                output_queue.push_back(operator_stack.back());
+                operator_stack.pop_back();
+            }
+            if(!operator_stack.empty() && operator_stack.back().type == L_PARENTHESIS){
+                operator_stack.pop_back();
+            }            
 
-            
+        }else if(token.type == STAR || token.type == CONCAT || token.type == UNION){
+            while(!operator_stack.empty() && operator_stack.back().type != L_PARENTHESIS
+            && getPrecedence(operator_stack.back().type) >= getPrecedence(token.type)){
 
+                output_queue.push_back(operator_stack.back());
+                operator_stack.pop_back();
+            }
+            operator_stack.push_back(token);
         }
+        // clean remaining shit in the operator stack onto the operator queue to finish postfix
+        while(!operator_stack.empty()){
+            output_queue.push_back(operator_stack.back());
+            operator_stack.pop_back();
+        }
+        return output_queue;
     }
     
 }
